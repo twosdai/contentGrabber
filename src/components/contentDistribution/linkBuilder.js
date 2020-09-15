f1a8eada788c026255667dd69813bedf42979b48
Daniel Wasserlauf <dwasserlauf@commercehub.com>
2020-09-15 10:38:34 -0400

    [master]: style change and added preload script

--- src/components/contentDistribution/linkBuilder.js
+++ src/components/contentDistribution/linkBuilder.js
@@ -1,3 +1,3 @@
-const buildHref = (link, title) => `<br><a href=${link}>${title}</a>`;
+const buildHref = (link, title) => `<br><a href=${link} style="font-size: 100px; text-decoration: none">${title}</a><br>`;
 
 module.exports = { buildHref };
--- src/components/contentDistribution/pageBuilder.js
+++ src/components/contentDistribution/pageBuilder.js
@@ -6,8 +6,10 @@ const pageBuilder = (arrayOfLinksAndTitles) => {
     const body = arrayOfLinksAndTitles.reduce((acc, { url: link, title }) => {
         return `${acc}${buildHref(link, title)}`;
     }, '');
-    const html = `<!DOCTYPE html><html><head>${header}</head><body>${body}</body></html>`;
+    const html = `<!DOCTYPE html><html><head>${header}</head>${preloadScript()}<body>${body}</body></html>`;
     return html;
 };
-
+// +
+const preloadScript = () =>
+    '<script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"></script>';
 module.exports = { pageBuilder };
